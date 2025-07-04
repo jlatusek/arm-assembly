b41870abae536384b922826f34ecee37ecbe39d3
Jakub Latusek <jk.latusek@gmail.com>
2025-07-04 07:40:00 +0200

    ch2: add printer and main

---
+++ chapter2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define GREETING "Hello"
+
+int main(int argc, char *argv[]) {
+  printf("%s ", GREETING);
+  for (int i = 1; i < argc; i++) {
+    printf("%s", argv[i]);
+    if (i != argc - 1) {
+      printf(", ");
+    }
+  }
+
+  printf("\n");
+
+  return 0;
+}
---
+++ chapter2/print.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define GREETING "Hello"
+
+int main(int argc, char *argv[]) {
+  printf(GREETING);
+
+  return 0;
+}
