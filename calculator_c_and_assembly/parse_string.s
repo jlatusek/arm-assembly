871dd9f1bcf747578406c8c3f2942152aa182c72
Jakub Latusek <jk.latusek@gmail.com>
2025-07-31 22:43:36 +0200

    simple-calc: add sceleton to print string
    i only print one charater using putc

--- calculator_c_and_assembly/parse_string.s
+++ calculator_c_and_assembly/parse_string.s
@@ -3,6 +3,9 @@
 .cpu cortex-a7
 .code	16
 
+.extern putc
+.extern stdout
+
 .thumb_func
 // int parse_string(char *str, unsigned int len, int *a, int *b, char *operator);
 // r0 = str
@@ -22,3 +25,12 @@ parse_string:
   movs r5, #'+'
   str R5, [r7]
   pop {r4, r5, r6, r7, pc}
+
+.global print_string
+print_string:
+    push {r3, r4, lr}
+    ldrb    r0, [r1, #1]!
+    ldr     r1, =stdout
+    bl      putc
+    pop {r3, r4, pc}
+
